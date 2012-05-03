8afca5024938e129bdb1a6bdb6f047f8386cf0ae
pluto <blueabysm@gmail.com>
2012-05-03 17:05:38 +0800

    fix php-mode parentheses indentations

--- custom.el
+++ custom.el
@@ -70,3 +70,9 @@
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/site-lisp/auto-complete/ac-dict")
 (ac-config-default)
+
+;; php-mode indentation in parentheses
+(add-hook 'php-mode-hook
+          (lambda ()
+            (c-set-offset 'arglist-intro '+)
+            (c-set-offset 'arglist-close 0)))
