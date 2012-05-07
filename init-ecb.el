d5481a5a4cfb13932bbcfcb3097ecc10342bf0d4
pluto <blueabysm@gmail.com>
2012-05-07 17:19:21 +0800

    add keyboard shortcuts to activate/deactivate ecb

--- init-ecb.el
+++ init-ecb.el
@@ -3,4 +3,9 @@
 (setq stack-trace-on-error t)
 
 (require 'ecb)
+
+;;active/deactive shortcuts
+(global-set-key (kbd "C-c C-a") 'ecb-activate)
+(global-set-key (kbd "C-c C-d") 'ecb-deactivate)
+
 (provide 'init-ecb)
