5db9aeeedc64b8789d20beda90abf3bf1348901c
Lei Wang <blueabysm@gmail.com>
2014-11-26 15:59:05 +0800

    modify key bindings

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -3,7 +3,7 @@
 (global-set-key (kbd "M-`") 'other-window)
 (global-set-key (kbd "C-c C-/") 'vr/replace)
 (global-set-key (kbd "C-c g") 'goto-line)
-(global-set-key (kbd "M-p") 'scroll-other-window)
-(global-set-key (kbd "M-n") 'scroll-other-window-down)
+(global-set-key (kbd "M-p") 'scroll-other-window-down)
+(global-set-key (kbd "M-n") 'scroll-other-window)
 
 (provide 'init-keybindings)
